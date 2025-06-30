ea40edc436aa5741d083be5b6e1e3d0996a47cb4
Daffa <daffakurama10@gmail.com>
2025-06-30 22:37:32 +0700

    fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,16 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(UsersTableSeeder::class);
         $this->call(GuruTendikTableSeeder::class);
         $this->call(BeritasTableSeeder::class);
         $this->call(KeluhKesahTableSeeder::class);
         $this->call(PrestasisTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
+        
     }
 }
